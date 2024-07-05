892c81e9e59720d55948d4960cc920ad38ed8fee
siddhartha8916 <siddhartha8916@gmail.com>
2024-07-05 17:29:08 +0530

    fix : updated headers

--- src/lib/axios-instance/app-survey-axios-instance.ts
+++ src/lib/axios-instance/app-survey-axios-instance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getApplicationSecret } from "../utils";
 
 const appSurveyApplicationInstance = axios.create({
   baseURL: "https://pwa-api.brainstacktechnologies.com",
@@ -8,5 +9,16 @@ const appSurveyApplicationInstance = axios.create({
   },
   timeout: 1000 * 60 * 2,
 });
+
+appSurveyApplicationInstance.interceptors.request.use(async function (config) {
+  // Do something before request is sent
+  const customSecretHeader = await getApplicationSecret();
+  config.headers["X-Custom-Header"] = customSecretHeader
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
 
 export default appSurveyApplicationInstance;
