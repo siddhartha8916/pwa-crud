006dad969a4c88ce33ddb37bd96bbf748db16b90
siddhartha8916 <siddhartha8916@gmail.com>
2024-07-05 17:15:35 +0530

    fix : updated axios instance

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,21 @@ import "./App.css";
 import { ReactQueryProvider } from "./lib/react-query-provider.tsx";
 import Router from "./routes/router.tsx";
 import { LanguageProvider } from "./context/language-context.tsx";
+import { useEffect } from "react";
+import appSurveyApplicationInstance from "./lib/axios-instance/app-survey-axios-instance.ts";
+import { getApplicationSecret } from "./lib/utils.ts";
 
 function App() {
+  useEffect(() => {
+    const setCustomHeader = async () => {
+      const customSecretHeader = await getApplicationSecret();
+      appSurveyApplicationInstance.defaults.headers.common["X-Custom-Header"] =
+        customSecretHeader;
+    };
+
+    setCustomHeader();
+  }, []);
+
   return (
     <ReactQueryProvider>
       <LanguageProvider>
